d49ea10860501b4170dffd943515a4e60990bc52
hanson-hason <160970937+hanson-hason@users.noreply.github.com>
2024-03-12 14:48:41 -0600

    Add files via upload

---
+++ jabascribd.js
@@ -0,0 +1,29 @@
+//Frayz Cumpleeter 9000 by Hanson
+
+//BUTTUN WUN
+document.getElementById("buttun1").addEventListener("click", act);
+
+function act() {
+alert("get you're act tugedder");
+}
+
+//BUTTUN TU
+document.getElementById("buttun2").addEventListener("click", cuntri);
+
+function cuntri() {
+alert("de best cuntri on erf is SINGAPORE.");
+}
+
+//BUTTUN THRI
+document.getElementById("buttun3").addEventListener("click", wurldz);
+
+function wurldz() {
+alert("get de best uv bouf wurldz.");
+}
+
+//BUTTUN FOR
+document.getElementById("buttun4").addEventListener("click", horse);
+
+function horse() {
+alert("im so tiyerd i kud eet a horse.");
+}
